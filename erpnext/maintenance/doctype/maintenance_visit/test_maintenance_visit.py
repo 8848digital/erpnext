6acae95f5a899d7bf5bd100c4314ecb30d35816f
hrishikesh8848 <158465727+hrishikesh8848@users.noreply.github.com>
2025-06-03 18:16:52 +0530

    Update test_maintenance_visit.py

--- erpnext/maintenance/doctype/maintenance_visit/test_maintenance_visit.py
+++ erpnext/maintenance/doctype/maintenance_visit/test_maintenance_visit.py
@@ -5,7 +5,6 @@ import unittest
 import frappe
 from frappe.utils.data import today
 
-from erpnext.accounts.doctype.payment_entry.test_payment_entry import create_customer
 from erpnext.stock.doctype.item.test_item import create_item
 
 # test_records = frappe.get_test_records('Maintenance Visit')
@@ -14,6 +13,7 @@ from erpnext.stock.doctype.item.test_item import create_item
 class TestMaintenanceVisit(unittest.TestCase):
 	def setUp(self):
 		from erpnext.accounts.doctype.payment_entry.test_payment_entry import create_company
+		from erpnext.accounts.doctype.payment_entry.test_payment_entry import create_customer
 
 		create_company()
 		self.customer = create_customer("_Test Customer", currency="INR")
