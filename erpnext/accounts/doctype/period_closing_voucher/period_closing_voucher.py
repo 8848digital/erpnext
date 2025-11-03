79277275e21cb131215d3827a0259bfc9554fb17
ruthra kumar <ruthra@erpnext.com>
2025-11-03 16:44:26 +0530

    refactor: minor changes on status
    1. set to 'In Progress' on start of both legacy and new controller
    2. force delete to avoid permission issues
    3. default to 1hr timeout
    
    (cherry picked from commit 9c13edc0b9e34c28a067871f260ebef866640558)

--- erpnext/accounts/doctype/period_closing_voucher/period_closing_voucher.py
+++ erpnext/accounts/doctype/period_closing_voucher/period_closing_voucher.py
@@ -128,8 +128,8 @@ class PeriodClosingVoucher(AccountsController):
 			frappe.throw(_("Currency of the Closing Account must be {0}").format(company_currency))
 
 	def on_submit(self):
+		self.db_set("gle_processing_status", "In Progress")
 		if frappe.get_single_value("Accounts Settings", "use_legacy_controller_for_pcv"):
-			self.db_set("gle_processing_status", "In Progress")
 			self.make_gl_entries()
 		else:
 			ppcv = frappe.get_doc({"doctype": "Process Period Closing Voucher", "parent_pcv": self.name})
@@ -162,7 +162,7 @@ class PeriodClosingVoucher(AccountsController):
 			"Process Period Closing Voucher", {"parent_pcv": self.name, "docstatus": ["in", [1, 2]]}
 		)
 		for x in ppcvs:
-			frappe.delete_doc("Process Period Closing Voucher", x.name)
+			frappe.delete_doc("Process Period Closing Voucher", x.name, force=True, ignore_permissions=True)
 
 	def cancel_process_pcv_docs(self):
 		ppcvs = frappe.db.get_all("Process Period Closing Voucher", {"parent_pcv": self.name, "docstatus": 1})
