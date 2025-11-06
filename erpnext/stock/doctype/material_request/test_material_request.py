d7f99f8a423247f0bb42fd1d116e434de5385fd6
Pugazhendhi Velu <pugazhendhi720@gmail.com>
2025-11-06 20:06:07 +0000

    test: add test for validate mr item qty against so with over-receipt allowance
    (cherry picked from commit 55f531bad620836ae2d5fd79903dbcabaafb737d)

--- erpnext/stock/doctype/material_request/test_material_request.py
+++ erpnext/stock/doctype/material_request/test_material_request.py
@@ -9287,6 +9287,18 @@ class TestMaterialRequest(FrappeTestCase):
 		pl_for_pending = create_pick_list(mr.name)
 		self.assertEqual(pl_for_pending.locations[0].qty, 5)
 
+	def test_material_request_qty_over_sales_order_limit(self):
+		from erpnext.controllers.status_updater import OverAllowanceError
+		from erpnext.selling.doctype.sales_order.test_sales_order import make_sales_order
+
+		so = make_sales_order()
+		mr = make_material_request(qty=100, do_not_submit=True)
+		mr.items[0].sales_order = so.name
+		mr.items[0].sales_order_item = so.items[0].name
+		mr.save()
+
+		self.assertRaises(OverAllowanceError, mr.submit)
+
 
 def get_in_transit_warehouse(company):
 	if not frappe.db.exists("Warehouse Type", "Transit"):
