fabdd1880bd41855777b14446ff3f75f3fb6761a
Kavin <78342682+kavin0411@users.noreply.github.com>
2025-11-07 11:16:58 +0530

    fix: handle partial dn against reserved stock
    (cherry picked from commit 9d979e34abbc99045b0d11464db8150d11ce5172)

--- erpnext/stock/doctype/serial_and_batch_bundle/serial_and_batch_bundle.py
+++ erpnext/stock/doctype/serial_and_batch_bundle/serial_and_batch_bundle.py
@@ -251,6 +251,9 @@ class SerialandBatchBundle(Document):
 		if self.voucher_type == "Delivery Note":
 			kwargs["ignore_voucher_nos"] = self.get_sre_against_dn()
 
+		if self.voucher_type == "Delivery Note":
+			kwargs["ignore_voucher_nos"] = self.get_sre_against_dn()
+
 		available_serial_nos = get_available_serial_nos(frappe._dict(kwargs))
 
 		serial_no_warehouse = {}
@@ -1386,6 +1389,20 @@ class SerialandBatchBundle(Document):
 			return sre_names
 		return None
 
+
+	def get_sre_against_dn(self):
+		from erpnext.stock.doctype.stock_reservation_entry.stock_reservation_entry import (
+			get_sre_against_so_for_dn,
+		)
+
+		so_name, so_detail_no = frappe.db.get_value(
+			"Delivery Note Item", self.voucher_detail_no, ["against_sales_order", "so_detail"]
+		)
+
+		if so_name and so_detail_no:
+			sre_names = get_sre_against_so_for_dn(so_name, so_detail_no)
+
+			return sre_names
 
 	def get_sre_against_dn(self):
 		from erpnext.stock.doctype.stock_reservation_entry.stock_reservation_entry import (
