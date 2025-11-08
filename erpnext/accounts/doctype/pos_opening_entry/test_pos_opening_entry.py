7061fc9191fe78a0703b49bc2de8a44a40b2e651
diptanilsaha <diptanil@frappe.io>
2025-11-08 12:31:40 +0530

    fix: prevent pos opening entry creation for disabled pos profile
    (cherry picked from commit e35e8968f0825256e09b7beba8615270b9559e4d)

--- erpnext/accounts/doctype/pos_opening_entry/test_pos_opening_entry.py
+++ erpnext/accounts/doctype/pos_opening_entry/test_pos_opening_entry.py
@@ -7,170 +7,103 @@ from erpnext.accounts.doctype.payment_entry.test_payment_entry import create_cus
 from erpnext.accounts.doctype.pos_closing_entry.pos_closing_entry import make_closing_entry_from_opening
 
 
-class TestPOSOpeningEntry(FrappeTestCase):
+class TestPOSOpeningEntry(IntegrationTestCase):
+	@classmethod
+	def setUpClass(cls):
+		frappe.db.sql("delete from `tabPOS Opening Entry`")
+		cls.enterClassContext(cls.change_settings("POS Settings", {"invoice_type": "POS Invoice"}))
+
+	@classmethod
+	def tearDownClass(cls):
+		frappe.db.sql("delete from `tabPOS Opening Entry`")
+
+	def setUp(self):
+		# Make stock available for POS Sales
+		frappe.db.sql("delete from `tabPOS Opening Entry`")
+		make_stock_entry(target="_Test Warehouse - _TC", qty=2, basic_rate=100)
+		from erpnext.accounts.doctype.pos_closing_entry.test_pos_closing_entry import init_user_and_profile
+
+		self.init_user_and_profile = init_user_and_profile
+
 	def tearDown(self):
-		frappe.db.rollback()
+		frappe.set_user("Administrator")
+		frappe.db.sql("delete from `tabPOS Profile`")
 
-	def test_pos_opening_to_pos_closing_TC_S_099(self):
-		create_customer("_Test Customer", currency="INR")
-		from erpnext.accounts.doctype.pos_closing_entry.test_pos_closing_entry import init_user_and_profile
-		from erpnext.accounts.doctype.pos_invoice.test_pos_invoice import create_pos_invoice
+	def test_pos_opening_entry(self):
+		test_user, pos_profile = self.init_user_and_profile()
+		opening_entry = create_opening_entry(pos_profile, test_user.name)
 
-		test_user, pos_profile = init_user_and_profile()
+		self.assertEqual(opening_entry.status, "Open")
+		self.assertNotEqual(opening_entry.docstatus, 0)
 
-		opening_entry = create_opening_entry(pos_profile=pos_profile, user=test_user.name)
+	def test_pos_opening_entry_on_disabled_pos(self):
+		test_user, pos_profile = self.init_user_and_profile(disabled=1)
+
+		with self.assertRaises(frappe.ValidationError):
+			create_opening_entry(pos_profile, test_user.name)
+
+	def test_multiple_pos_opening_entries_for_same_pos_profile(self):
+		test_user, pos_profile = self.init_user_and_profile()
+		opening_entry = create_opening_entry(pos_profile, test_user.name)
+
+		self.assertEqual(opening_entry.status, "Open")
+		with self.assertRaises(frappe.ValidationError):
+			create_opening_entry(pos_profile, test_user.name)
+
+	def test_multiple_pos_opening_entry_for_multiple_pos_profiles(self):
+		test_user, pos_profile = self.init_user_and_profile()
+		opening_entry_1 = create_opening_entry(pos_profile, test_user.name)
+
+		self.assertEqual(opening_entry_1.status, "Open")
+		self.assertEqual(opening_entry_1.user, test_user.name)
+
+		cashier_user = create_user("test_cashier@example.com", "Accounts Manager", "Sales Manager")
+		frappe.set_user(cashier_user.name)
+
+		pos_profile2 = make_pos_profile(name="_Test POS Profile 2")
+		opening_entry_2 = create_opening_entry(pos_profile2, cashier_user.name)
+
+		self.assertEqual(opening_entry_2.status, "Open")
+		self.assertEqual(opening_entry_2.user, cashier_user.name)
+
+	def test_multiple_pos_opening_entry_for_same_pos_profile_by_multiple_user(self):
+		test_user, pos_profile = self.init_user_and_profile()
+		cashier_user = create_user("test_cashier@example.com", "Accounts Manager", "Sales Manager")
+
+		opening_entry = create_opening_entry(pos_profile, test_user.name)
 		self.assertEqual(opening_entry.status, "Open")
 
-		pos_inv1 = create_pos_invoice(rate=3500, do_not_submit=1)
-		pos_inv1.append("payments", {"mode_of_payment": "Cash", "account": "Cash - _TC", "amount": 3500})
-		pos_inv1.paid_amount = pos_inv1.grand_total
-		pos_inv1.outstanding_amount = 0
+		with self.assertRaises(frappe.ValidationError):
+			create_opening_entry(pos_profile, cashier_user.name)
+
+	def test_user_assignment_to_multiple_pos_profile(self):
+		test_user, pos_profile = self.init_user_and_profile()
+		opening_entry_1 = create_opening_entry(pos_profile, test_user.name)
+		self.assertEqual(opening_entry_1.user, test_user.name)
+
+		pos_profile2 = make_pos_profile(name="_Test POS Profile 2")
+		with self.assertRaises(frappe.ValidationError):
+			create_opening_entry(pos_profile2, test_user.name)
+
+	def test_cancel_pos_opening_entry_without_invoices(self):
+		test_user, pos_profile = self.init_user_and_profile()
+		opening_entry = create_opening_entry(pos_profile, test_user.name, get_obj=True)
+
+		opening_entry.cancel()
+		self.assertEqual(opening_entry.status, "Cancelled")
+		self.assertNotEqual(opening_entry.docstatus, 1)
+
+	def test_cancel_pos_opening_entry_with_invoice(self):
+		test_user, pos_profile = self.init_user_and_profile()
+		opening_entry = create_opening_entry(pos_profile, test_user.name, get_obj=True)
+
+		pos_inv1 = create_pos_invoice(pos_profile=pos_profile.name, rate=100, do_not_save=1)
+		pos_inv1.append("payments", {"mode_of_payment": "Cash", "account": "Cash - _TC", "amount": 100})
+		pos_inv1.save()
 		pos_inv1.submit()
 
-		pos_inv2 = create_pos_invoice(rate=3200, do_not_submit=1)
-		pos_inv2.append("payments", {"mode_of_payment": "Cash", "account": "Cash - _TC", "amount": 3200})
-		pos_inv2.paid_amount = pos_inv2.grand_total
-		pos_inv2.outstanding_amount = 0
-		pos_inv2.submit()
+		self.assertRaises(frappe.ValidationError, opening_entry.cancel)
 
-		closing_enrty = make_closing_entry_from_opening(opening_entry)
-		closing_enrty.submit()
-		opening_entry.reload()
-		self.assertEqual(opening_entry.status, "Closed")
-
-	def test_pos_opening_to_closing_enrty_check_cashire_and_posprofile_TC_S_100(self):
-		from erpnext.accounts.doctype.pos_closing_entry.test_pos_closing_entry import init_user_and_profile
-		from erpnext.accounts.doctype.pos_invoice.test_pos_invoice import create_pos_invoice
-
-		test_user, pos_profile = init_user_and_profile()
-
-		opening_entry = create_opening_entry(pos_profile=pos_profile, user=test_user.name)
-		self.assertEqual(opening_entry.status, "Open")
-		self.assertEqual(opening_entry.pos_profile, pos_profile.name)
-		self.assertEqual(opening_entry.user, test_user.name)
-
-		pos_inv1 = create_pos_invoice(rate=3500, do_not_submit=1)
-		pos_inv1.append("payments", {"mode_of_payment": "Cash", "account": "Cash - _TC", "amount": 3500})
-		pos_inv1.paid_amount = pos_inv1.grand_total
-		pos_inv1.submit()
-
-		pos_inv2 = create_pos_invoice(rate=3200, do_not_submit=1)
-		pos_inv2.append("payments", {"mode_of_payment": "Cash", "account": "Cash - _TC", "amount": 3200})
-		pos_inv2.paid_amount = pos_inv2.grand_total
-		pos_inv2.outstanding_amount = 0
-		pos_inv2.submit()
-
-		closing_enrty = make_closing_entry_from_opening(opening_entry)
-		closing_enrty.submit()
-		opening_entry.reload()
-		self.assertEqual(opening_entry.status, "Closed")
-
-	def test_pos_opening_to_closing_enrty_with_opening_balance_TC_S_101(self):
-		from erpnext.accounts.doctype.pos_closing_entry.test_pos_closing_entry import init_user_and_profile
-		from erpnext.accounts.doctype.pos_invoice.test_pos_invoice import create_pos_invoice
-
-		test_user, pos_profile = init_user_and_profile()
-
-		opening_entry = frappe.new_doc("POS Opening Entry")
-		opening_entry.pos_profile = pos_profile.name
-		opening_entry.user = test_user.name
-		opening_entry.company = pos_profile.company
-		opening_entry.period_start_date = frappe.utils.get_datetime()
-
-		# Add opening balance details
-		balance_details = []
-		for d in pos_profile.payments:
-			balance_details.append({"mode_of_payment": d.mode_of_payment, "opening_amount": 1000})
-
-		opening_entry.set("balance_details", balance_details)
-		opening_entry.save()
-		opening_entry.submit()
-		self.assertEqual(opening_entry.status, "Open")
-
-		pos_inv1 = create_pos_invoice(rate=3500, do_not_submit=1)
-		pos_inv1.append("payments", {"mode_of_payment": "Cash", "account": "Cash - _TC", "amount": 3500})
-		pos_inv1.paid_amount = pos_inv1.grand_total
-		pos_inv1.outstanding_amount = 0
-		pos_inv1.submit()
-
-		pos_inv2 = create_pos_invoice(rate=3200, do_not_submit=1)
-		pos_inv2.append("payments", {"mode_of_payment": "Cash", "account": "Cash - _TC", "amount": 3200})
-		pos_inv2.paid_amount = pos_inv2.grand_total
-		pos_inv2.outstanding_amount = 0
-		pos_inv2.submit()
-
-		closing_enrty = make_closing_entry_from_opening(opening_entry)
-		closing_enrty.submit()
-		opening_entry.reload()
-		self.assertEqual(opening_entry.status, "Closed")
-
-	def test_validate_pos_profile_and_cashier_TC_ACC_318(self):
-		user = frappe.get_doc(
-			{
-				"doctype": "User",
-				"email": frappe.generate_hash(length=10) + "@gmail.com",
-				"first_name": frappe.generate_hash(length=5),
-				"send_welcome_email": 0,
-				"enabled": 0,
-			}
-		)
-		user.insert(ignore_permissions=True)
-		pos_profile = frappe.get_doc(
-			{
-				"doctype": "POS Profile",
-				"__newname": frappe.generate_hash(length=5),
-				"company": "_Test Company",
-				"warehouse": "Stores - _TC",
-				"write_off_account": "Sales - _TC",
-				"write_off_cost_center": "Main - _TC",
-				"write_off_limit": 1,
-				"payments": [{"default": 1, "mode_of_payment": "Cash"}],
-			}
-		)
-		pos_profile.insert()
-
-		poe = frappe.get_doc(
-			{
-				"doctype": "POS Opening Entry",
-				"company": "_Test Company 1",
-				"period_start_date": frappe.utils.now_datetime,
-				"posting_date": frappe.utils.today(),
-				"pos_profile": pos_profile.name,
-				"balance_details": [{"mode_of_payment": "Cash", "opening_amount": 100}],
-			}
-		)
-		with self.assertRaises(frappe.ValidationError) as cm:
-			poe.insert(ignore_mandatory=True, ignore_permissions=True)
-		self.assertIn(
-			f"POS Profile {pos_profile.name} does not belongs to company {poe.company}", str(cm.exception)
-		)
-
-		poe.company = pos_profile.company
-		poe.user = user.name
-		with self.assertRaises(frappe.ValidationError) as cm:
-			poe.insert()
-		self.assertIn(f"User {user.name} is disabled. Please select valid user/cashier", str(cm.exception))
-
-		user.enabled = 1
-		user.save()
-
-		poe_1 = frappe.get_doc(
-			{
-				"doctype": "POS Opening Entry",
-				"company": "_Test Company",
-				"period_start_date": frappe.utils.now_datetime,
-				"posting_date": frappe.utils.today(),
-				"pos_profile": pos_profile.name,
-				"user": user.name,
-				"balance_details": [{"mode_of_payment": get_mode_of_payment(), "opening_amount": 100}],
-			}
-		)
-		with self.assertRaises(frappe.ValidationError) as cm:
-			poe_1.insert()
-		self.assertIn(
-			"Please set default Cash or Bank account in Mode of Payments __Test Mode Payment",
-			str(cm.exception),
-		)
 
 
 def create_opening_entry(pos_profile, user):
