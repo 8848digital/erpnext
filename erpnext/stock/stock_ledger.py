2308cf1342863bc8ff72d3eb3da786d0884dba43
Kavin <78342682+kavin0411@users.noreply.github.com>
2025-11-10 14:28:37 +0530

    fix(stock): ignore current voucher in reserved stock validation

--- erpnext/stock/stock_ledger.py
+++ erpnext/stock/stock_ledger.py
@@ -2240,6 +2240,7 @@ def validate_reserved_stock(kwargs):
 	# ignore current voucher when validating the reserved stock
 	if not kwargs.ignore_voucher_nos and kwargs.voucher_no:
 		kwargs.ignore_voucher_nos = [kwargs.voucher_no]
+
 	if kwargs.serial_no:
 		validate_reserved_serial_nos(kwargs)
 
