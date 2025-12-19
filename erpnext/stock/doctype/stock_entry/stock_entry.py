1686fa8ecbb1b9be71eb1d4b94fc6112286cf840
Smit Vora <smitvora203@gmail.com>
2025-12-19 14:07:09 +0530

    fix: de-duplicate rows on disassembly with multiple manufacture entries
    (cherry picked from commit a091e47bd7589bda083c0d411f5e518ca12ac0fd)

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -1927,6 +1927,7 @@ class StockEntry(StockController):
 				["Stock Entry Detail", "docstatus", "=", 1],
 			],
 			order_by="`tabStock Entry Detail`.`idx` desc, `tabStock Entry Detail`.`is_finished_item` desc",
+			group_by="`tabStock Entry Detail`.`item_code`",
 		)
 
 	@frappe.whitelist()
