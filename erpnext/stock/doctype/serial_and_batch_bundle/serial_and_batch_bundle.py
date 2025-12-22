07bfb833b750d10a60f8537f1b02d51abcd266c6
Rohit Waghchaure <rohitw1991@gmail.com>
2025-12-22 12:04:40 +0530

    fix: limit condition to fetch serial nos
    (cherry picked from commit da4b78491d5da659c300bacb7bf964204bd8fb15)
    
    # Conflicts:
    #	erpnext/stock/get_item_details.py

--- erpnext/stock/doctype/serial_and_batch_bundle/serial_and_batch_bundle.py
+++ erpnext/stock/doctype/serial_and_batch_bundle/serial_and_batch_bundle.py
@@ -1983,9 +1983,9 @@ def get_available_serial_nos(kwargs):
 
 	order_by = "creation"
 	if kwargs.based_on == "LIFO":
-		order_by = "creation desc"
+		order_by = "creation"
 	elif kwargs.based_on == "Expiry":
-		order_by = "amc_expiry_date asc"
+		order_by = "amc_expiry_date"
 
 	filters = {"item_code": kwargs.item_code}
 
@@ -2032,7 +2032,12 @@ def get_serial_nos_based_on_filters(filters, fields, order_by, kwargs):
 	doctype = frappe.qb.DocType("Serial No")
 
 	order_by_column = getattr(doctype, order_by)
-	query = frappe.qb.from_(doctype).orderby(order_by_column).limit(cint(kwargs.qty) or 10000000).for_update()
+	query = frappe.qb.from_(doctype).limit(cint(kwargs.qty) or 10000000).for_update()
+
+	if kwargs.based_on == "LIFO":
+		query = query.orderby(order_by_column, order=frappe.query_builder.Order.desc)
+	else:
+		query = query.orderby(order_by_column)
 
 	for key, value in filters.items():
 		column = getattr(doctype, key)
