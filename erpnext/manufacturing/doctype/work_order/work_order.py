a104961761156d36401c873a06f72fdc134103d3
Sudharsanan11 <sudharsananashok1975@gmail.com>
2026-01-18 23:03:24 +0530

    fix(manufacturing): consider process loss qty while validating the work order
    (cherry picked from commit e6366e830c8d21279ea666209f46b1164caa2466)

--- erpnext/manufacturing/doctype/work_order/work_order.py
+++ erpnext/manufacturing/doctype/work_order/work_order.py
@@ -307,8 +307,8 @@ class WorkOrder(Document):
 	def validate_work_order_against_so(self):
 		# already ordered qty
 		ordered_qty_against_so = frappe.db.sql(
-			"""select sum(qty) from `tabWork Order`
-			where production_item = %s and sales_order = %s and docstatus < 2 and name != %s""",
+			"""select sum(qty - process_loss_qty) from `tabWork Order`
+			where production_item = %s and sales_order = %s and docstatus < 2 and status != 'Closed' and name != %s""",
 			(self.production_item, self.sales_order, self.name),
 		)[0][0]
 
