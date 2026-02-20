a0fe697ee1bc1e20e2689d08ff2d05cb8c85164b
Mihir Kandoi <kandoimihir@gmail.com>
2026-02-20 14:21:26 +0530

    fix: update items fetches wrong item code
    (cherry picked from commit ba96d37c11ea06906f9177229a32e152c48ff390)

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -671,7 +671,7 @@ erpnext.utils.update_child_items = function (opts) {
 					filters: filters,
 				};
 			},
-			onchange: function () {
+			change: function () {
 				const me = this;
 				frm.call({
 					method: "erpnext.stock.get_item_details.get_item_details",
