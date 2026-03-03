6b1880dbda762d532f71cf1fc69d4f7cf8c5b541
kavin-114 <kavinkannan892@gmail.com>
2026-03-03 19:41:50 +0530

    fix(selling): handle selling price validation for FG item
    System checks valuation rate in incoming_rate field, since SO has it in valuation_rate field of item row,
    need to handle the field name dynamically based upon the doctype name in Selling Controller.
    
    (cherry picked from commit 433531848259db472715f9f8991a4537c1a75710)

--- erpnext/controllers/selling_controller.py
+++ erpnext/controllers/selling_controller.py
@@ -296,10 +296,7 @@ class SellingController(StockController):
 					bold(ref_rate_field),
 					bold("net rate"),
 					bold(rate),
-<<<<<<< HEAD
-=======
 					bold(frappe.get_meta("Selling Settings").get_label("validate_selling_price")),
->>>>>>> a61ad15998 (fix: add missing param)
 					get_link_to_form("Selling Settings", "Selling Settings"),
 				),
 				title=_("Invalid Selling Price"),
@@ -329,45 +326,12 @@ class SellingController(StockController):
 			if is_internal_customer or not is_stock_item:
 				continue
 
-			valuation_rate_map[(item.item_code, item.warehouse)] = None
-
-		if not valuation_rate_map:
-			return
-
-		or_conditions = (
-			f"""(item_code = {frappe.db.escape(valuation_rate[0])}
-			and warehouse = {frappe.db.escape(valuation_rate[1])})"""
-			for valuation_rate in valuation_rate_map
-		)
-
-		valuation_rates = frappe.db.sql(
-			f"""
-			select
-				item_code, warehouse, valuation_rate
-			from
-				`tabBin`
-			where
-				({" or ".join(or_conditions)})
-				and valuation_rate > 0
-		""",
-			as_dict=True,
-		)
-
-		for rate in valuation_rates:
-			valuation_rate_map[(rate.item_code, rate.warehouse)] = rate.valuation_rate
-
-		for item in self.items:
-			if not item.item_code or item.is_free_item:
-				continue
-
-			last_valuation_rate = valuation_rate_map.get((item.item_code, item.warehouse))
-
-			if not last_valuation_rate:
-				continue
-
-			last_valuation_rate_in_sales_uom = last_valuation_rate * (item.conversion_factor or 1)
-
-			if flt(item.base_net_rate) < flt(last_valuation_rate_in_sales_uom):
+			rate_field = "valuation_rate" if self.doctype in ["Sales Order", "Quotation"] else "incoming_rate"
+			if item.get(rate_field) and item.base_net_rate < (
+				valuation_rate := flt(
+					item.get(rate_field) * (item.conversion_factor or 1), item.precision("base_net_rate")
+				)
+			):
 				throw_message(
 					item.idx,
 					item.item_name,
