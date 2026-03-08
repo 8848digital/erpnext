710241cf0befdfe5cc6510f21c8aa620f5c6781a
Pandiyan37 <pandiyanpalani37@gmail.com>
2026-03-09 01:14:24 +0530

    fix(selling): update delivery date in line items
    (cherry picked from commit 77367b55171f31e366f90f72255bcbc738610487)

--- erpnext/selling/doctype/sales_order/sales_order.js
+++ erpnext/selling/doctype/sales_order/sales_order.js
@@ -47,6 +47,13 @@ frappe.ui.form.on("Sales Order", {
 		frm.set_df_property("packed_items", "cannot_add_rows", true);
 		frm.set_df_property("packed_items", "cannot_delete_rows", true);
 	},
+	delivery_date(frm) {
+		if (frm.doc.delivery_date) {
+			frm.doc.items.forEach((d) => {
+				frappe.model.set_value(d.doctype, d.name, "delivery_date", frm.doc.delivery_date);
+			});
+		}
+	},
 
 	refresh: function (frm) {
 		if (frm.doc.docstatus === 1) {
@@ -134,7 +141,7 @@ frappe.ui.form.on("Sales Order", {
 				});
 			}
 		}
-
+		prevent_past_delivery_dates(frm);
 		// Hide `Reserve Stock` field description in submitted or cancelled Sales Order.
 		if (frm.doc.docstatus > 0) {
 			frm.set_df_property("reserve_stock", "description", null);
@@ -220,13 +227,6 @@ frappe.ui.form.on("Sales Order", {
 			"Unreconcile Payment",
 			"Unreconcile Payment Entries",
 		];
-	},
-
-	delivery_date: function (frm) {
-		$.each(frm.doc.items || [], function (i, d) {
-			if (!d.delivery_date) d.delivery_date = frm.doc.delivery_date;
-		});
-		refresh_field("items");
 	},
 
 	create_stock_reservation_entries(frm) {
@@ -1396,3 +1396,11 @@ erpnext.selling.SalesOrderController = class SalesOrderController extends erpnex
 };
 
 extend_cscript(cur_frm.cscript, new erpnext.selling.SalesOrderController({ frm: cur_frm }));
+
+function prevent_past_delivery_dates(frm) {
+	if (frm.doc.transaction_date) {
+		frm.fields_dict["delivery_date"].datepicker?.update({
+			minDate: new Date(frm.doc.transaction_date),
+		});
+	}
+}
