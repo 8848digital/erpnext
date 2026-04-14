5ea2a875b3a7a3f45496d303ad1c9e47e91932ea
Sameer Waman Bhoir <sameer@8848digital.com>
2026-04-14 15:01:47 +0600

    fix: resolve patch issues and refactor queries for PostgreSQL compatibility

--- erpnext/patches/v15_0/replace_http_with_https_in_sales_partner.py
+++ erpnext/patches/v15_0/replace_http_with_https_in_sales_partner.py
@@ -4,7 +4,14 @@ from pypika.functions import Replace
 
 
 def execute():
-	sp = frappe.qb.DocType("Sales Partner")
-	qb.update(sp).set(sp.partner_website, Replace(sp.partner_website, "http://", "https://")).where(
-		sp.partner_website.rlike("^http://.*")
-	).run()
+    sp = frappe.qb.DocType("Sales Partner")
+
+    (
+        qb.update(sp)
+        .set(
+            sp.partner_website,
+            Replace(sp.partner_website, "http://", "https://")
+        )
+        .where(sp.partner_website.like("http://%"))
+        .run()
+    )
