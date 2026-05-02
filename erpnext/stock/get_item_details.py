fd2f9fc0b99ca4047b70e38767db542ea5b7c984
Kaajalchhattani <89331214+Kaajalchhattani@users.noreply.github.com>
2026-05-02 21:23:03 +0530

    fix: set valid_from in created Item Price (#54696)
    Co-authored-by: Kaajal-Chhattani <kaajal.chhattani@aurigait.com>

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -1032,6 +1032,7 @@ def insert_item_price(args):
 				currency=args.currency,
 				uom=args.stock_uom,
 				price_list=args.price_list,
+				valid_from=transaction_date,
 			)
 			item_price.insert()
 			frappe.msgprint(
@@ -1056,6 +1057,7 @@ def insert_item_price(args):
 				"currency": args.currency,
 				"price_list_rate": price_list_rate,
 				"uom": args.stock_uom,
+				"valid_from": transaction_date,
 			}
 		)
 		item_price.insert()
